de7d13a5c42b2bb03a8898acd5edc2bf326c97f9
KimEunA97 <kimeuna9712@gmail.com>
2023-07-26 14:17:04 +0900

    공식문서 api 호출 코드

---
+++ fetch.js
@@ -0,0 +1,26 @@
+const { json } = require("stream/consumers");
+
+fetch('https://api.tarkov.dev/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({query: `{
+    items(name: "m855a1") {
+        id
+        name
+        shortName
+    }
+}`})
+})
+  .then(r => r.json())
+  .then(data => console.log('data returned:', data))
+
+
+// const url = 'https://api.tarkov.dev/graphql'
+// fetch(url)
+//   .then((res) => console.log(res))
+//   .then((data) => JSON.stringify(data))
+//   .then((data) => console.dir(data))
+
