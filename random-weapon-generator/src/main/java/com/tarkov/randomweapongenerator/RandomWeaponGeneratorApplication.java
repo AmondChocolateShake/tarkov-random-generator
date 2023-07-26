42affb12db6dfeda757bb215bf6f620066c547aa
AmondChocolateShake <dongju230130@gmail.com>
2023-07-26 17:22:02 +0900

    spring boot setup

---
+++ random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/RandomWeaponGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.tarkov.randomweapongenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomWeaponGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomWeaponGeneratorApplication.class, args);
+	}
+
+}
---
+++ random-weapon-generator/src/test/java/com/tarkov/randomweapongenerator/RandomWeaponGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarkov.randomweapongenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomWeaponGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
