997f53dff475f0405899d52bf0f75382fe942326
AmondChocolateShake <dongju230130@gmail.com>
2023-07-29 15:45:11 +0900

    next.js SSR 연습

--- view/pages/index.jsx
+++ view/pages/index.jsx
@@ -1,13 +1,25 @@
 import Link from "next/link";
 
 const App=()=>{
+    const username=user&&user.name;
 
-    return(
-        <div>
-            <h2>Link to tomato page</h2>
-            <Link href='/tomato'>
-                <a>move to '/tomato'</a>
-            </Link>
-        </div>
-    )
+    return <div>{username}</div>
 }
+
+export const getServerSideProps=async()=>{
+    try{
+        const res=await fetch("https://api.github.com/users/AmondChocolateShake");
+        if(res.status==200){
+            const user=await res.json();
+            return {props:{user}}
+        }
+        return{props:{}};
+    }catch(e){
+        console.log(e);
+        return{props:{}};
+    }
+};
+
+
+
+export default App;
