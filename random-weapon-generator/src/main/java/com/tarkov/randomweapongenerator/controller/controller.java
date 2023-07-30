e386d76274b0b11d5f8a02837682e63408558129
AmondChocolateShake <dongju230130@gmail.com>
2023-07-30 20:24:01 +0900

    #6 컨트롤러 내 총기 생성 옵션 박스의 옵션 상태를 저장할 Option 클래스 생성

--- random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/controller/controller.java
+++ random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/controller/controller.java
@@ -1,5 +1,34 @@
 package com.tarkov.randomweapongenerator.controller;
 
+
+// 총기 부품 생성 옵션 박스 내 체크 박스 상태 저장 클래스
+class Option{
+    private boolean operability;
+    private boolean noScope;
+    private boolean noStock;
+    private boolean noMagezine;
+    private boolean noHandguard;
+    private boolean noForegrip;
+    private boolean noPistolgrip;
+    private boolean noBullet;
+
+    protected Option(boolean operability, boolean noScope, boolean noStock, boolean noMagezine, boolean noHandguard, boolean noForegrip, boolean noPistolgrip, boolean noBullet ){
+        this.operability=operability;
+        this.noScope=noScope;
+        this.noStock=noStock;
+        this.noMagezine=noMagezine;
+        this.noHandguard=noHandguard;
+        this.noForegrip=noForegrip;
+        this.noPistolgrip=noPistolgrip;
+        this.noBullet=noBullet;
+    }
+
+    protected boolean[] getter(){
+        return new boolean[] {operability, noScope, noStock, noMagezine, noHandguard, noForegrip, noPistolgrip, noBullet};
+    }
+
+}
+
 public class controller {
     
 }
