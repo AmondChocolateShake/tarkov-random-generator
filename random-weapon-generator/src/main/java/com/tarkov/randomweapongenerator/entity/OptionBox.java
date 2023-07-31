00671f5a1caa207246796b7bacd275825b578ecc
AmondChocolateShake <dongju230130@gmail.com>
2023-07-31 10:44:41 +0900

    #7 option box에 사용될 옵션 상태를 저장할 클래스에 setter를 추가하는 중이고 OptionIds라는 enum 상수를 추가해 값으로 활용할 예정이다.

--- random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/entity/OptionBox.java
+++ random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/entity/OptionBox.java
@@ -6,31 +6,45 @@ public class OptionBox{
     
     
 }
+
+
+
 // 총기 부품 생성 옵션 박스 내 체크 박스 상태 저장 클래스
 class Option{
     private boolean operability;
     private boolean noScope;
     private boolean noStock;
-    private boolean noMagezine;
+    private boolean noMagazine;
     private boolean noHandguard;
     private boolean noForegrip;
     private boolean noPistolgrip;
     private boolean noBullet;
 
-    protected Option(boolean operability, boolean noScope, boolean noStock, boolean noMagezine, boolean noHandguard, boolean noForegrip, boolean noPistolgrip, boolean noBullet ){
+
+    // 옵션 박스 내 옵션 체크박스 상태 값 저장 생성자
+    // 모든 옵션 상태값을 입력 받아야함.
+    // boolean 값만 허용
+    protected Option(boolean operability, boolean noScope, boolean noStock, boolean noMagazine, boolean noHandguard, boolean noForegrip, boolean noPistolgrip, boolean noBullet ){
         this.operability=operability;
         this.noScope=noScope;
         this.noStock=noStock;
-        this.noMagezine=noMagezine;
+        this.noMagazine=noMagazine;
         this.noHandguard=noHandguard;
         this.noForegrip=noForegrip;
         this.noPistolgrip=noPistolgrip;
         this.noBullet=noBullet;
     }
 
+
+    // 모든 옵션 값을 리턴하는 함수
     protected boolean[] getOptionStates(){
-        return new boolean[] {operability, noScope, noStock, noMagezine, noHandguard, noForegrip, noPistolgrip, noBullet};
+        return new boolean[] {operability, noScope, noStock, noMagazine, noHandguard, noForegrip, noPistolgrip, noBullet};
     }
 
+
+    // 옵션 값을 id로 취급하여
+    protected void setOptionStateById(){
+
+    }
 
 }
---
+++ random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/entity/OptionIds.java
@@ -0,0 +1,13 @@
+package com.tarkov.randomweapongenerator.entity;
+
+
+public enum OptionIds{
+    OPERABILITY,
+    NoSCOPE,
+    NoSTOCK,
+    NoMAGAZINE,
+    NoHANDGUARD,
+    NoFOREGRIP,
+    NoPISTOLGRIP,
+    NoBULLET
+}
