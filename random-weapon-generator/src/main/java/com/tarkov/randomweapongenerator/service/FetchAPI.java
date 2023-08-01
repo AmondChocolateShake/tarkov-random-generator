6fb47e42ce7d80f4ce2f5317c49e123c4c7fad2a
AmondChocolateShake <dongju230130@gmail.com>
2023-08-01 19:18:36 +0900

    application.properties에 graphql endpoint 추가

--- random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/service/FetchAPI.java
+++ random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/service/FetchAPI.java
@@ -1,19 +1,33 @@
 package com.tarkov.randomweapongenerator.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 // to fetch API
 // API url required
+@Service
 public class FetchAPI {
-    String APIurl;
-    WebClient webClient;
 
-    public FetchAPI(String url){
-        APIurl=url;
-        webClient=WebClient.create(APIurl);
+    @Value("${graphql.endpoint}")
+    private String GraphQLurl;
+    private WebClient webClient=WebClient.create();
+
+
+
+    protected Object SelectDataByWeaponId(String id){
+        
+        "query {
+            items(categoryNames:Weapon){
+              name
+              categories{
+                name
+              }
+            }
+          }"
     }
-
-    
 
 }
 
