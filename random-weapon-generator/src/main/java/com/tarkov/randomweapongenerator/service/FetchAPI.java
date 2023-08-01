2b7446a819f30567bb7a46c9558a741a6e970cee
AmondChocolateShake <dongju230130@gmail.com>
2023-08-01 18:28:33 +0900

    FetchAPI url을 생성자로 받고 해당 url로 WebClient 생성

--- random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/service/FetchAPI.java
+++ random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/service/FetchAPI.java
@@ -1,13 +1,19 @@
 package com.tarkov.randomweapongenerator.service;
 
+import org.springframework.web.reactive.function.client.WebClient;
 
 // to fetch API
 // API url required
 public class FetchAPI {
+    String APIurl;
+    WebClient webClient;
 
     public FetchAPI(String url){
-        
+        APIurl=url;
+        webClient=WebClient.create(APIurl);
     }
+
+    
 
 }
 
