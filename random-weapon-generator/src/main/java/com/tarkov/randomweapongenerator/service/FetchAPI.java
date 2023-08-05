d49db8491b4d99a759a78e5cbb87e14262c9e3fd
AmondChocolateShake <dongju230130@gmail.com>
2023-08-05 15:19:48 +0900

    #11 Annotation test : RequestMappint GET to fetch API

--- random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/service/FetchAPI.java
+++ random-weapon-generator/src/main/java/com/tarkov/randomweapongenerator/service/FetchAPI.java
@@ -3,14 +3,17 @@ package com.tarkov.randomweapongenerator.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 // to fetch API
 // API url required
-@Service
+@Component
 public class FetchAPI {
 
     private String GraphQLurl="https://api.tarkov.dev/graphql";
@@ -22,7 +25,8 @@ public class FetchAPI {
     }
 
 
-    public void SelectDataByWeaponId(String id){
+    @RequestMapping(value ="/tarkov", method =RequestMethod.GET )
+    public String SelectDataByWeaponId(String id){
         String query="{\"query\":\"{ item( id :\\\""+id+"\\\"){ name id }}\"}";
         System.out.println(query);
         String response=webClient.post()
@@ -32,7 +36,7 @@ public class FetchAPI {
             .retrieve()
             .bodyToMono(String.class)
             .block();
-        
+        return response;
     }
 
 }
