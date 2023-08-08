6a62164a5c22acb9f6baa1f4be7a087e7563fc47
KimEunA97 <kimeuna9712@gmail.com>
2023-08-09 02:29:43 +0900

    자꾸 hydration 오류 나서 일단 전역 관리 파일 제거해봄

--- view/pages/_app.jsx
+++
@@ -1,6 +0,0 @@
-// _app.jsx은 Next.js에서 공식적으로 전역 상태 관리 기능을 제공하도록 만들어놓은 파일이다.
-
-import '../src/styles/styles.css'
-export default function App({Component, pageProps}){
-    return <Component {...pageProps} />
-}
