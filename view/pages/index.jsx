c1375b6210ea3385237cfbc723eec53cbf5da069
KimEunA97 <kimeuna9712@gmail.com>
2023-08-09 02:24:10 +0900

    body 스타일 적용

--- view/pages/index.jsx
+++ view/pages/index.jsx
@@ -1,12 +1,12 @@
 import Link from "next/link"
 import ModeListBox from "../src/components/modeListBox";
 import React from "react";
-
+import style from '../styles/styles.module.css'
 
 const App = () => {
 
     return (
-        <body>
+        <body className={style.body}>
             <div>
                 <Link href='/' legacyBehavior>
                     <a><h1>Escape from Tarkov random weapon generator</h1></a>
