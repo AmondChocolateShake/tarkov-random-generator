294e28e14bf80496e34ddabfce75b793b1ef6f33
KimEunA97 <kimeuna9712@gmail.com>
2023-08-09 02:57:29 +0900

    hydration 에러 해결 필요

--- view/src/components/modeListBox.jsx
+++ view/src/components/modeListBox.jsx
@@ -1,24 +1,11 @@
 import React from "react";
-import css from 'styled-jsx/css'
+// import ''
 
 const modeListBox = () => {
 
   return (
     <div>
       <h2>modeListBox</h2>
-      <style>
-        {
-          `
-          div {
-            width : 80%;
-            height : 50%;
-          }
-          h2 {
-  color: white;
-          }
-          `
-        }
-      </style>
     </div>
   )
 
